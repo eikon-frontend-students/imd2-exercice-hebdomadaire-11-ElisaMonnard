06cc6109b638b961637e90d4d9c3dd9e73b0cf7c
ElisaMo <Elisa.Monnard01@studentfr.ch>
2026-01-18 20:08:45 +0100

    transformer texte en nombre

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,9 @@
 var togglebtn = document.querySelector(".togglebtn");
 var number = document.querySelector(".count");
+let counter = Number(number.textContent);
 togglebtn.addEventListener("click", function () {
   togglebtn.classList.toggle("is-active");
   number.classList.toggle("is-active");
-
   setTimeout(() => {
     togglebtn.classList.remove("is-active");
     number.classList.remove("is-active");
