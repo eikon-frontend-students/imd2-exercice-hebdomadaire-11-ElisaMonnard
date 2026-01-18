6ef2e2405f5336a3fde672518ef93903c51055c9
ElisaMo <Elisa.Monnard01@studentfr.ch>
2026-01-18 19:57:39 +0100

    JS fonctionnel

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,6 @@
-var button = document.querySelector(".button");
+var togglebtn = document.querySelector(".togglebtn");
+var number = document.querySelector(".count");
+togglebtn.addEventListener("click", function () {
+  togglebtn.classList.toggle("is-active");
+  number.classList.toggle("is-active");
+});
