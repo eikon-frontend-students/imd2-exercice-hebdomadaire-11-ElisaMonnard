9a8073b3744d172f868144e175fdf2dbad1cb830
ElisaMo <Elisa.Monnard01@studentfr.ch>
2026-01-18 20:06:47 +0100

    la couleur redevient normale après 2 secondes

--- src/js/main.js
+++ src/js/main.js
@@ -3,4 +3,9 @@ var number = document.querySelector(".count");
 togglebtn.addEventListener("click", function () {
   togglebtn.classList.toggle("is-active");
   number.classList.toggle("is-active");
+
+  setTimeout(() => {
+    togglebtn.classList.remove("is-active");
+    number.classList.remove("is-active");
+  }, 2000);
 });
